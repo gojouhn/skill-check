e5de8100327289667e3dfef6b7d330525a321b4c
gojouhn <gojouhn@yahoo.co.jp>
2020-04-30 18:28:01 +0900

    7,9実施

--- src/main/java/q007/Q007.java
+++ src/main/java/q007/Q007.java
@@ -1,30 +1,112 @@
 package q007;
 
+import java.util.ArrayList;
+
 /**
  * q007 最短経路探索
- *
+ * <p>
  * 壁を 'X' 通路を ' ' 開始を 'S' ゴールを 'E' で表現された迷路で、最短経路を通った場合に
  * 何歩でゴールまでたどり着くかを出力するプログラムを実装してください。
  * もし、ゴールまで辿り着くルートが無かった場合は -1 を出力してください。
  * なお、1歩は上下左右のいずれかにしか動くことはできません（斜めはNG）。
- *
+ * <p>
  * 迷路データは MazeInputStream から取得してください。
  * 迷路の横幅と高さは毎回異なりますが、必ず長方形（あるいは正方形）となっており、外壁は全て'X'で埋まっています。
  * 空行が迷路データの終了です。
- *
-
-[迷路の例]
-XXXXXXXXX
-XSX    EX
-X XXX X X
-X   X X X
-X X XXX X
-X X     X
-XXXXXXXXX
-
-[答え]
-14
+ * <p>
+ * <p>
+ * [迷路の例]
+ * XXXXXXXXX
+ * XSX    EX
+ * X XXX X X
+ * X   X X X
+ * X X XXX X
+ * X X     X
+ * XXXXXXXXX
+ * <p>
+ * [答え]
+ * 14
  */
 public class Q007 {
+    public static void main(String[] args) {
+        MazeInputStream mazeInputStream = new MazeInputStream();
+        Integer[][] data = new Integer[50][50];
+        int i = 0;
+        int j = 0;
+        ArrayList<Integer[]> position = new ArrayList<>();
+        while (true) {
+            int maze = mazeInputStream.read();
+            if (maze < 0) {
+                break;
+            }
+            switch ((char) maze) {
+                case 'X':
+                    data[i][j] = -1;
+                    i++;
+                    break;
+                case 'S':
+                    data[i][j] = 0;
+                    Integer[] pos = {i, j};
+                    position.add(pos);
+                    i++;
+                    break;
+                case 'E':
+                    data[i][j] = 999;
+                    i++;
+                    break;
+                case '\n':
+                    j++;
+                    i = 0;
+                    break;
+                default:
+                    i++;
+                    break;
+            }
+        }
+
+        for (int step = 1; step < 999; step++) {
+            ArrayList<Integer[]> nextPosition = new ArrayList<>();
+            for (Integer[] pos : position) {
+                Integer dataPos1 = data[pos[0] + 1][pos[1]];
+                Integer dataPos2 = data[pos[0]][pos[1] + 1];
+                Integer dataPos3 = data[pos[0] - 1][pos[1]];
+                Integer dataPos4 = data[pos[0]][pos[1] - 1];
+                if ((data[pos[0] + 1][pos[1]] != null && data[pos[0] + 1][pos[1]] == 999)
+                        || (data[pos[0] - 1][pos[1]] != null && data[pos[0] - 1][pos[1]] == 999)
+                        || (data[pos[0]][pos[1] + 1] != null && data[pos[0]][pos[1] + 1] == 999)
+                        || (data[pos[0]][pos[1] - 1] != null && data[pos[0]][pos[1] - 1] == 999)) {
+                    System.out.println(step);
+                    return;
+                }
+                if (data[pos[0] + 1][pos[1]] == null) {
+                    data[pos[0] + 1][pos[1]] = step;
+                    Integer[] nextPos = {pos[0] + 1, pos[1]};
+                    nextPosition.add(nextPos);
+                }
+                if (data[pos[0]][pos[1] + 1] == null) {
+                    data[pos[0]][pos[1] + 1] = step;
+                    Integer[] nextPos = {pos[0], pos[1] + 1};
+                    nextPosition.add(nextPos);
+                }
+                if (data[pos[0] - 1][pos[1]] == null) {
+                    data[pos[0] - 1][pos[1]] = step;
+                    Integer[] nextPos = {pos[0] - 1, pos[1]};
+                    nextPosition.add(nextPos);
+                }
+                if (data[pos[0]][pos[1] - 1] == null) {
+                    data[pos[0]][pos[1] - 1] = step;
+                    Integer[] nextPos = {pos[0], pos[1] - 1};
+                    nextPosition.add(nextPos);
+                }
+            }
+            if (nextPosition.isEmpty()) {
+                System.out.println("-1");
+                return;
+            }
+            position = (ArrayList<Integer[]>) nextPosition.clone();
+        }
+
+    }
+
 }
 // 完成までの時間: xx時間 xx分
--- src/main/java/q009/Q009.java
+++ src/main/java/q009/Q009.java
@@ -1,26 +1,103 @@
 package q009;
 
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 /**
  * Q009 重い処理を別スレッドで実行
- *
+ * <p>
  * 標準入力から整数を受け取り、別スレッドで素因数分解して、その整数を構成する2以上の素数を求めるプログラムを記述してください。
  * - 素因数分解は重い処理であるため、別スレッドで実行してください
  * - 標準入力から整数を受け取った後は、再度標準入力に戻ります
  * - 空文字が入力された場合は、素因数分解を実行中の状態を表示します（「実行中」あるいは処理結果を表示）
  * - 素因数分解の効率的なアルゴリズムを求めるのが問題ではないため、あえて単純なアルゴリズムで良い（遅いほどよい）
- *   （例えば、2および3以上の奇数で割り切れるかを全部チェックする方法でも良い）
+ * （例えば、2および3以上の奇数で割り切れるかを全部チェックする方法でも良い）
  * - BigIntegerなどを使って、大きな数値も扱えるようにしてください
-[実行イメージ]
-入力) 65536
-入力)
-65536: 実行中  <-- もし65536の素因数分解が実行中だった場合はこのように表示する
-入力) 12345
-入力)
-65536: 2      <-- 実行が終わっていたら結果を表示する。2の16乗だが、16乗の部分の表示は不要（複数溜まっていた場合の順番は問わない）
-12345: 実行中
-入力)
-12345: 3,5,823
+ * [実行イメージ]
+ * 入力) 65536
+ * 入力)
+ * 65536: 実行中  <-- もし65536の素因数分解が実行中だった場合はこのように表示する
+ * 入力) 12345
+ * 入力)
+ * 65536: 2      <-- 実行が終わっていたら結果を表示する。2の16乗だが、16乗の部分の表示は不要（複数溜まっていた場合の順番は問わない）
+ * 12345: 実行中
+ * 入力)
+ * 12345: 3,5,823
  */
 public class Q009 {
+
+    public static void main(String[] args) {
+        BigInteger bi;
+        String input = "";
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals("end")) {
+                return;
+            }
+            if (input.isEmpty()) {
+                BigInteger inputBi = MultiThread.getMultiThreadInt();
+                StringBuilder sb = MultiThread.getSb();
+                if (sb == null || sb.length() == 0) {
+                    System.out.println(inputBi + ": 実行中");
+                } else {
+                    System.out.println(inputBi + ": " + sb);
+                }
+            } else {
+                bi = new BigInteger(input);
+                MultiThread mt = new MultiThread(bi);
+                mt.start();
+            }
+        }
+
+    }
+}
+
+class MultiThread extends Thread {
+    //変数の宣言
+    private static BigInteger multiThreadInt;
+    private static StringBuilder sb;
+
+    public static BigInteger getMultiThreadInt() {
+        return multiThreadInt;
+    }
+
+    public static StringBuilder getSb() {
+        return sb;
+    }
+
+    //スレッド作成時に実行される処理
+    public MultiThread(BigInteger bi){
+        this.multiThreadInt = bi;
+        this.sb = new StringBuilder();
+    }
+
+    public void run() {
+        BigInteger num = this.multiThreadInt;
+        Set<BigInteger> numSet = new HashSet<>();
+
+        while (num.remainder(BigInteger.valueOf(2)).equals(BigInteger.ZERO)) {
+            num = num.divide(BigInteger.valueOf(2));
+            numSet.add(BigInteger.valueOf(2));
+        }
+
+        BigInteger insu = BigInteger.valueOf(3);
+
+        while (num.compareTo(BigInteger.ONE) > 0) {
+            while (num.remainder(insu).equals(BigInteger.ZERO)) {
+                num = num.divide(insu);
+                numSet.add(insu);
+            }
+            insu = insu.add(BigInteger.valueOf(2));
+        }
+        for (BigInteger a : numSet) {
+            sb.append(a.toString()).append(",");
+        }
+        sb.setLength(sb.length()-1);
+    }
 }
 // 完成までの時間: xx時間 xx分
